0f9715c14c2cfb9be545642bdd0cceb0eb1c502a
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2020-04-18 16:09:19 +0300

    major diff

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,18 @@
 package calc
 
+import "errors"
+
 // returns sum of two integers
-func Add(numbers ...int) int {
+func Add(numbers ...int) (int, error) {
 	sum := 0
 
-	for _, num := range numbers {
-		sum += num
+	if len(numbers) < 2 {
+		return sum, errors.New("provide more than 2 numbers")
+	} else {
+		for _, num := range numbers {
+			sum += num
+		}
 	}
 
-	return sum
+	return sum, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/developer-guy/nummanip
+module github.com/developer-guy/nummanip/v2
 
 go 1.14
